f2ea4c2d06e962e1ed04f49b260c377b362d2e82
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-22 17:45:55 -0300

    Added more comments.

--- bin/index.js
+++ bin/index.js
@@ -39,26 +39,31 @@ function loop () {
 
 // Check if there's something outdated to publish
 function checkOutdated (type) {
+  // Make field name and time to be considered as outdated
   let fieldName = `sources.facebook.fanpages.latestSync.${type}`
   let latestTime = new Date(Date.now() - TIME_OUTDATED)
 
+  // Make filters from the dynamic field name
   let exists = {}
   exists[fieldName] = { $exists: false }
 
   let outdated = {}
   outdated[fieldName] = { $lt: latestTime }
 
-  let filters = {}
-  filters['sources.facebook.fanpages._id'] = 1
-  filters[fieldName] = 1
+  // Select only fanpage ID
+  let select = {
+    'sources.facebook.fanpages._id': 1
+  }
 
-  Site.find({ $or: [ exists, outdated ] }, filters, (err, docs) => {
+  Site.find({ $or: [ exists, outdated ] }, select, (err, docs) => {
     docs.map((i) => {
+      // Map outdated fanpages to the message format
       return {
         type: type,
         object_id: i.sources.facebook.fanpages[0]._id
       }
     }).forEach((i) => {
+      // Publish each message to the proper Redis channel
       let data = JSON.stringify(i)
       client.publish(CHANNEL_NAME, data)
       log.info(`Published message to "${CHANNEL_NAME}: "${data}".`)

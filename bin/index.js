eceadb05453bbe5ffda1328c446cb3d1699f37ee
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-23 13:02:45 -0300

    Making posts and albums back dependents.

--- bin/index.js
+++ bin/index.js
@@ -33,13 +33,14 @@ loop()
 // Service loop
 function loop () {
   checkOutdated('about')
-  checkOutdated('posts')
+  checkOutdated('posts', 'about')
+  checkOutdated('albums', 'posts')
 
   setTimeout(loop, 60 * 1000)
 }
 
 // Check if there's something outdated to publish
-function checkOutdated (type) {
+function checkOutdated (type, depends) {
   // Make field name and time to be considered as outdated
   let fieldName = `sources.facebook.fanpages.latestSync.${type}`
   let latestTime = new Date(Date.now() - TIME_OUTDATED)
@@ -51,25 +52,42 @@ function checkOutdated (type) {
   let outdated = {}
   outdated[fieldName] = { $lt: latestTime }
 
+  let filter = { $or: [ exists, outdated ] }
+
+  // Some fields depends on another
+  if (depends) {
+    let dependName = `sources.facebook.fanpages.latestSync.${depends}`
+    let another = {}
+    another[dependName] = { $gt: latestTime }
+
+    filter = {
+      $and: [ another, { $or: [ exists, outdated] } ]
+    }
+  }
+
   // Select only fanpage ID
   let select = {
     'sources.facebook.fanpages._id': 1
   }
 
   // Find outdated websites for the given field
-  Site.find({ $or: [ exists, outdated ] }, select, (err, docs) => {
-    docs.map((i) => {
-      // Map outdated fanpages to the message format
-      return {
-        siteId: i._id,
-        objectId: i.sources.facebook.fanpages[0]._id,
-        type: type
-      }
-    }).forEach((i) => {
-      // Publish each message to the proper Redis channel
-      let data = JSON.stringify(i)
-      client.publish(CHANNEL_NAME, data)
-      log.info(`Published message to "${CHANNEL_NAME}: "${data}".`)
-    })
+  Site.find(filter, select, (err, docs) => {
+    if (err) {
+      log.error(err)
+    } else {
+      docs.map((i) => {
+        // Map outdated fanpages to the message format
+        return {
+          siteId: i._id,
+          objectId: i.sources.facebook.fanpages[0]._id,
+          type: type
+        }
+      }).forEach((i) => {
+        // Publish each message to the proper Redis channel
+        let data = JSON.stringify(i)
+        client.publish(CHANNEL_NAME, data)
+        log.info(`Published message to "${CHANNEL_NAME}: "${data}".`)
+      })
+    }
   })
 }
